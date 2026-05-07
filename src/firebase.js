7cf90ffb7336a2a9ca3fb30121ea07380ea25a74
erabus49-debug <erabus49@gmail.com>
2026-05-08 00:52:40 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyACVqDohRcepGPAMa0loRbVmVwbtNhbkNQ",
   authDomain: "car-stock-system.firebaseapp.com",
   projectId: "car-stock-system",
-  storageBucket: "car-stock-system.firebasestorage.app",
+  storageBucket: "car-stock-system.appspot.com",
   messagingSenderId: "326293663025",
   appId: "1:326293663025:web:81662b16f10e0229e91410",
 };
